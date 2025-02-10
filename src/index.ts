2a7eb3487ff3f281a18466b0fe3408600f7666c3
Billy Charlton <mail@billyc.cc>
2025-02-10 18:05:14 +0100

    fix UTF-8 BOM parsing errors - some files have BOM as first bytes
    0xFEFF is the "BOM" marker that Windows annoyingly puts at the front
    of some text files. These are unnecessary and break the node file loading

--- src/index.ts
+++ src/index.ts
@@ -21,9 +21,13 @@ const loadCSV = async (network: GMNSNetwork, element: string): Promise<any[]> =>
   const fullPath = upath.joinSafe(network.path, `${element}.csv`)
 
   try {
-    const text = loadFileSync(fullPath)
+    let text = loadFileSync(fullPath)
+
+    // fix UTF-8 BOM
+    if (text.charCodeAt(0) === 0xfeff) text = text.slice(1)
+
     const parsed = Papa.parse(text, {
-      delimitersToGuess: ['\t', ';', ',', ' '],
+      delimitersToGuess: [',', '\t', ';', ' '],
       comments: '#',
       skipEmptyLines: true,
       dynamicTyping: true,
@@ -56,8 +60,12 @@ export const load = async (path: string): Promise<GMNSNetwork> => {
     el: {},
   }
 
+  // get config: if no config, default to long/lat
+  const config = await loadCSV(network, 'config')
+  if (config.length) network.config = config[0]
+  else network.config = { crs: 4326 }
+
   // get standard things
-  network.config = (await loadCSV(network, 'config'))[0]
   network.el.node = await loadCSV(network, 'node')
   network.el.link = await loadCSV(network, 'link')
   network.el.geometry = await loadCSV(network, 'geometry')
@@ -65,62 +73,69 @@ export const load = async (path: string): Promise<GMNSNetwork> => {
   return network
 }
 
-const generateGeometry = (geom: any) => {
-  return wktToGeoJSON(geom.geometry)
-}
-
 export const toGeojson = (network: GMNSNetwork) => {
   // build node coord lookup
   const nodeLookup = {} as any
-  for (const node of network.el.node) nodeLookup[node.node_id] = node
+  for (const node of network.el.node) {
+    // this trim is required because BOM at filestart will break first column name
+    nodeLookup[node.node_id] = node
+  }
+  // console.error(111, nodeLookup)
+
   // build geometry lookup
   const geomLookup = {} as any
-  if (network.el.geometry) {
+  if (network.el.geometry.length) {
+    console.error('doing the geoms')
     for (const geom of network.el.geometry) geomLookup[geom.geometry_id] = geom
   }
+
   const features = [] as any[]
   const crs = Number.isFinite(network.config.crs)
     ? `EPSG:${network.config.crs}`
     : network.config.crs
 
   for (const link of network.el.link) {
-    const nFrom = nodeLookup[link.from_node_id]
-    const nTo = nodeLookup[link.to_node_id]
+    try {
+      const nFrom = nodeLookup[link.from_node_id]
+      const nTo = nodeLookup[link.to_node_id]
+      // console.error({ nFrom, nTo, linkFrom: link.from_node_id, linkTo: link.to_node_id })
 
-    // create link geometry
-    let geometry
-    if (link.geometry_id in geomLookup) {
-      geometry = generateGeometry(geomLookup[link.geometry_id])
-    } else {
-      geometry = {
-        type: 'LineString',
-        coordinates: [
-          [nFrom.x_coord, nFrom.y_coord],
-          [nTo.x_coord, nTo.y_coord],
-        ],
+      // create link geometry
+      let geometry
+      if (link.geometry_id && link.geometry_id in geomLookup) {
+        geometry = wktToGeoJSON(geomLookup[link.geometry_id])
+      } else {
+        geometry = {
+          type: 'LineString',
+          coordinates: [
+            [nFrom.x_coord, nFrom.y_coord],
+            [nTo.x_coord, nTo.y_coord],
+          ],
+        }
       }
+
+      const feature = {
+        type: 'Feature',
+        id: link.link_id,
+        geometry,
+        properties: link,
+      } as any
+
+      delete feature.properties.geometry
+
+      // convert coord to EPSG:4326 long/lat if necessary
+      if (crs !== 'EPSG:4326') {
+        coordEach(feature, currentCoord => {
+          let newCoord = Coords.toLngLat(crs, currentCoord)
+          // console.error(newCoord)
+          currentCoord[0] = newCoord[0]
+          currentCoord[1] = newCoord[1]
+        })
+      }
+      features.push(feature)
+    } catch (e) {
+      console.error(`Error processing Link id ${link.link_id}`)
     }
-
-    const feature = {
-      type: 'Feature',
-      id: link.link_id,
-      geometry,
-      properties: link,
-    } as any
-
-    delete feature.properties.geometry
-
-    // convert coord to EPSG:4326 long/lat if necessary
-    if (crs !== 'EPSG:4326') {
-      coordEach(feature, currentCoord => {
-        let newCoord = Coords.toLngLat(crs, currentCoord)
-        // console.error(newCoord)
-        currentCoord[0] = newCoord[0]
-        currentCoord[1] = newCoord[1]
-      })
-    }
-
-    features.push(feature)
   }
 
   return {
