206e430dc90cc46c0049f2c4677df46b646d240c
Billy Charlton <mail@billyc.cc>
2025-02-11 13:42:01 +0100

    update test

--- src/__tests__/index.test.ts
+++ src/__tests__/index.test.ts
@@ -4,7 +4,3 @@ import { greet, toGeojson } from '../index'
 test('greet returns correct greeting', () => {
   expect(greet('World')).toBe('Hello, World!')
 })
-
-test('toGeojson fails if no path given', () => {
-  expect(toGeojson('')).toBe({})
-})
